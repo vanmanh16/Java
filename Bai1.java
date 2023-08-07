a36e1e5b6559ee8392982c0f7364d64976c35bb2
vanmanh16 <vanmanh0913@gmail.com>
2023-08-07 10:43:40 +0700

    first commit

---
+++ Bai1.java
@@ -0,0 +1,14 @@
+public class Bai1 {
+   public static void main(String[] args) {
+      System.out.println("Sau day la mot so thong tin ve Van Manh:");
+      int tuoi;
+      String ten; // Chữ string phải viết hoa chữ S
+      String hocvan;
+      tuoi = 19;
+      ten = "Duong Van Manh";
+      hocvan = "Truong Dai hoc Cong nghiep Ha Noi";
+      System.out.println("Tuoi: " + tuoi);
+      System.out.println("Ten: " + ten); // System phải viết hoa chữ S
+      System.out.println("Hoc van: " + hocvan);
+   }
+}
